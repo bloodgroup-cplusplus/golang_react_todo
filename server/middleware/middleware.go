c6c72d1202c574a4f77f5634b26e9872c0910a12
bloodgroup-cplusplus <tucater10896601@protonmail.com>
2023-10-02 20:40:49 +0530

    creating database middleware

--- server/middleware/middleware.go
+++ server/middleware/middleware.go
@@ -1,1 +1,47 @@
 package middleware
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/joho/godotenv"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+var collection *mongo.Collection
+
+func init() {
+	loadTheEnv()
+	//	createDBInstance()
+}
+
+func loadTheEnv() {
+	err := godotenv.Load("env")
+	if err != nil {
+		log.Fatal("Error loading the .env file")
+	}
+}
+
+func GetAllTasks(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func CreateTask() {
+
+}
+
+func TaskComplete() {
+
+}
+
+func UndoTask() {
+
+}
+
+func DeleteTask() {
+
+}
+
+func deleteAllTasks() {
+
+}
