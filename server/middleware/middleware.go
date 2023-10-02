66838f91f94ff0c4d4d9ae3da95101d5d1548904
bloodgroup-cplusplus <tucater10896601@protonmail.com>
2023-10-02 20:52:04 +0530

    adding database middlewares

--- server/middleware/middleware.go
+++ server/middleware/middleware.go
@@ -1,18 +1,32 @@
 package middleware
 
 import (
+	"context"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/joho/godotenv"
 	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	//"go.mongodb.org/mongo-driver/bson"
+	// "go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 var collection *mongo.Collection
 
 func init() {
 	loadTheEnv()
-	//	createDBInstance()
+	createDBInstance()
+}
+
+func createDBInstance() {
+	connectionString := os.Getenv("DB_URI")
+	dbName := os.Getenv("DB_NAME")
+	collName := os.Getenv("DB_COLLECTION_NAME")
+	clientOptions := options.Client().ApplyURL(connectionString)
+	client, err := mongo.Connect(context.TODO(), clientOptions)
+
 }
 
 func loadTheEnv() {
