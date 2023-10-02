192a1129b9b77978b3c0ffdf3f420956fae7ee40
bloodgroup-cplusplus <tucater10896601@protonmail.com>
2023-10-02 17:01:25 +0530

    adding first component

---
+++ client/src/To-Do-List.jsx
@@ -0,0 +1,32 @@
+//import axios from "axios"
+//import {Card,Header,Form,Input,Icon} from "semantic-ui-react"
+//import { useState,useEffect } from "react";
+import { Header } from "semantic-ui-react"
+
+//let endpoint = "http://localhost:9000";
+
+function ToDoList()
+{
+ //   const [task,setTask] = useState("")
+  //  const[items,setItems] = useState([])
+
+    //useEffect(()=>{
+
+
+    //},[])
+
+    return (
+        <div>
+            <div className="row">
+                <Header className="header" as="h2" color="yellow">
+                    TO DO LIST
+                </Header>
+            </div>
+        </div>
+
+    )
+
+}
+
+
+export default ToDoList
